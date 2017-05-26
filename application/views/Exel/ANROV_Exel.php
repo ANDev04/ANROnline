58fd71436f3a30de893136dadf4eaff3668fb985
arifnurdiansyah92 <arifnurdiansyah92@gmail.com>
2017-05-26 16:34:03 +0700

    Fix Ajax Nilai + Tampilan

--- application/views/Exel/ANROV_Exel.php
+++ application/views/Exel/ANROV_Exel.php
@@ -1,6 +1,18 @@
-<body>
-    <form autocomplete="off" method="post" action="<?php echo base_url("ANROC_Exel/Save") ?>" enctype="multipart/form-data">
-        <input type="hidden" name="table" value="<?php echo $table?>">
-        <input type="file" name="file"/>
-        <button type="submit" name="type" value="insert">Submit</button>
-    </form>
+<main>
+    <div class="container">
+        <div class="row">
+            <div class="col s12 z-depth-3">
+                <blockquote><h3>Import Excel</h3></blockquote>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col s12 z-depth-3 center">
+                <form autocomplete="off" method="post" action="<?php echo base_url("ANROC_Exel/Save") ?>" enctype="multipart/form-data">
+                    <input type="hidden" name="table" value="<?php echo $table?>">
+                    <input type="file" name="file"/>
+                    <button class="btn" type="submit" name="type" value="insert">Submit</button>
+                </form>
+            </div>
+        </div>
+    </div> 
+</main>
