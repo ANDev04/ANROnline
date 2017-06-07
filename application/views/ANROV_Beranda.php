d497ed21b891bec8cfe790d2f45358072d54503a
arifnurdiansyah92 <arifnurdiansyah92@gmail.com>
2017-06-07 11:20:24 +0700

    Menambah Desain Dashboard

--- application/views/ANROV_Beranda.php
+++ application/views/ANROV_Beranda.php
@@ -1,11 +1,53 @@
     <main>
         <div class="container">
-            <div class="row">
+            <div class="row z-depth-2">
                 <div class="col s12">
+                    <blockquote><h3>Dashboard</h3></blockquote>
+                    <hr>
+                </div>
+                <div class="col s6">
                     <div class="card">
-                        <blockquote class="flow-text">Selamat Datang di ANROnline!</blockquote>
+                        <div class="card-content center-align indigo lighten-3">
+                            <h1 class="blue-text"><?php echo $siswa ?></h1>
+                            <p>Data Siswa</p>
+                        </div>
+                        <div class="card-action indigo lighten-2">
+                            <a class="white-text" href="<?php echo base_url("ANROC_Siswa") ?>">Lihat Data Siswa</a>
+                        </div>
                     </div>
-                    
+                </div>
+                <div class="col s6">
+                    <div class="card">
+                        <div class="card-content center-align green lighten-3">
+                            <h1 class="green-text"><?php echo $pelajaran ?></h1>
+                            <p>Data Pelajaran</p>
+                        </div>
+                        <div class="card-action green lighten-2">
+                            <a class="white-text" href="<?php echo base_url("ANROC_Siswa") ?>">Lihat Data Pelajaran</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col s6">
+                    <div class="card">
+                        <div class="card-content center-align orange lighten-3">
+                            <h1 class="orange-text"><?php echo $guru ?></h1>
+                            <p>Data Guru</p>
+                        </div>
+                        <div class="card-action orange lighten-2">
+                            <a class="white-text" href="<?php echo base_url("ANROC_Siswa") ?>">Lihat Data Siswa</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col s6">
+                    <div class="card">
+                        <div class="card-content center-align purple lighten-3">
+                            <h1 class="purple-text"><?php echo $kelas ?></h1>
+                            <p>Data Siswa</p>
+                        </div>
+                        <div class="card-action purple lighten-2">
+                            <a class="white-text" href="<?php echo base_url("ANROC_Siswa") ?>">Lihat Data Siswa</a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
